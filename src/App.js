a10fd7427c75eb6453383864726447adddb1a972
Emeka Obianom <3mekslondon@gmail.com>
2022-10-24 09:53:28 +0100

    basic startup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React</h1>
+      <p>This is the best way to go</p>
+    </>
+  );
+};
+
+export default App;
